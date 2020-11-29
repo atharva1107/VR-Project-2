a3efaa6987f010a6a1ac0b7d724cd6fe1fc2b783
fpalting <faus.d.palting@ttu.edu>
2020-11-29 16:19:05 -0600

    update
    fixed script for getting objects into the basket.

---
+++ VR Project 2 Covid Shopping Simulator/Assets/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Player : MonoBehaviour
+{
+
+    public int maxExposure = 100;
+
+    public int currentExposure;
+
+    public ExposureMeter exposureMeter;
+
+    void Start()
+    {
+        currentExposure = maxExposure;
+        exposureMeter.setMaxExposure(maxExposure);
+        
+    }
+
+    void Update()
+    {
+
+        //This is for taking damage. anytime we calculate that distance between player and enemy and it's less than "6 feet" take continous damage
+        //
+        StartCoroutine(WaitForSeconds());
+    
+        
+    }
+    IEnumerator WaitForSeconds()
+    {
+        //TakeDamage(1);
+
+        yield return new WaitForSecondsRealtime(1);
+
+    }
+    void TakeDamage(int damage)
+    {
+        currentExposure -= damage;
+        exposureMeter.setExposure(currentExposure);
+
+    }
+
+}
---
+++ VR Project 2 Covid Shopping Simulator/Assets/basket.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic; // need this to use list
+
+public class basket : MonoBehaviour
+{
+
+
+    private List<string> boughtList = new List<string>();
+    private void OnDisable()//clears when the game or scene ends i think
+    {
+        /* just for checking if contents of the list are being added correctly
+        for(int i =0; i< boughtList.Count; i++)
+        {
+            Debug.Log(boughtList[i]);
+        }
+        */
+        boughtList.Clear();
+
+
+    }
+    private void OnTriggerEnter(Collider other) //when the object enters the trigger collider
+    {
+
+       
+        //if using a list of gameObject mesh name so if box.fbx -> box is used
+        var meshFilter = other.gameObject.GetComponent<MeshFilter>();
+        var mesh = meshFilter.sharedMesh;
+        boughtList.Add(mesh.name);
+    }
+
+    private void OnTriggerExit(Collider other)//same as above, but exiting
+    {
+
+
+
+
+        //despawns the object
+        if (other != null)
+        {
+            Destroy(other.gameObject.GetComponent<MeshRenderer>());
+        }
+
+       
+
+    }
+}
