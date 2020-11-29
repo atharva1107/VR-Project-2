c5e2d2f824d68824bd881afb8db8f53baf4548cf
fpalting <faus.d.palting@ttu.edu>
2020-11-29 12:00:29 -0600

    Added a canvas for damage indicator
    as above, but still trying to get it to show when wearing the headset (it shows up on the monitor).

---
+++ VR Project 2 Covid Shopping Simulator/Assets/ExposureMeter.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ExposureMeter : MonoBehaviour
+{
+
+    public Slider slider;
+    
+    public void setMaxExposure(int exposure)
+    {
+        slider.maxValue = exposure;
+        slider.value = exposure;
+    }
+    public void setExposure(int exposure)
+    {
+        slider.value = exposure;
+    }
+            
+}
