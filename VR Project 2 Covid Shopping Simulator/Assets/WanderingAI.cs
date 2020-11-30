d8b72ca1804dfd6314fc2bd7039c387e9cc4f444
fpalting <faus.d.palting@ttu.edu>
2020-11-29 22:28:50 -0600

    Fixed an issue with a model

--- VR Project 2 Covid Shopping Simulator/Assets/WanderingAI.cs
+++ VR Project 2 Covid Shopping Simulator/Assets/WanderingAI.cs
@@ -3,47 +3,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.AI;
 
+[RequireComponent(typeof(CharacterController))]
 
 public class WanderingAI : MonoBehaviour
-{
+{ 
+/// Creates wandering behaviour for a CharacterController.
+/// </summary>
 
-    public float wanderRadius;
-    public float wanderTimer;
+	public float speed = 5;
+	public float directionChangeInterval = 1;
+	public float maxHeadingChange = 30;
 
-    private Transform target;
-    private NavMeshAgent agent;
-    private float timer;
+	CharacterController controller;
+	float heading;
+	Vector3 targetRotation;
 
-    // Use this for initialization
-    void OnEnable()
-    {
-        agent = GetComponent<NavMeshAgent>();
-        timer = wanderTimer;
-    }
+	void Awake()
+	{
+		controller = GetComponent<CharacterController>();
 
-    // Update is called once per frame
-    void Update()
-    {
-        timer += Time.deltaTime;
+		// Set random initial rotation
+		heading = Random.Range(0, 360);
+		transform.eulerAngles = new Vector3(0, heading, 0);
 
-        if (timer >= wanderTimer)
-        {
-            Vector3 newPos = RandomNavSphere(transform.position, wanderRadius, -1);
-            agent.SetDestination(newPos);
-            timer = 0;
-        }
-    }
+		StartCoroutine(NewHeading());
+	}
 
-    public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
-    {
-        Vector3 randDirection = Random.insideUnitSphere * dist;
+	void Update()
+	{
+		transform.eulerAngles = Vector3.Slerp(transform.eulerAngles, targetRotation, Time.deltaTime * directionChangeInterval);
+		var forward = transform.TransformDirection(Vector3.forward);
+		controller.SimpleMove(forward * speed);
+	}
 
-        randDirection += origin;
+	/// <summary>
+	/// Repeatedly calculates a new direction to move towards.
+	/// Use this instead of MonoBehaviour.InvokeRepeating so that the interval can be changed at runtime.
+	/// </summary>
+	IEnumerator NewHeading()
+	{
+		while (true)
+		{
+			NewHeadingRoutine();
+			yield return new WaitForSeconds(directionChangeInterval);
+		}
+	}
 
-        NavMeshHit navHit;
-
-        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
-
-        return navHit.position;
-    }
+	/// <summary>
+	/// Calculates a new direction to move towards.
+	/// </summary>
+	void NewHeadingRoutine()
+	{
+		var floor = Mathf.Clamp(heading - maxHeadingChange, 0, 360);
+		var ceil = Mathf.Clamp(heading + maxHeadingChange, 0, 360);
+		heading = Random.Range(floor, ceil);
+		targetRotation = new Vector3(0, heading, 0);
+	}
 }
