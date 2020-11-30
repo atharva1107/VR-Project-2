551a9b9741a87e02d8e780e22515469b426f6b04
Atharva Raibagi <atharvaraibagi@gmail.com>
2020-11-29 20:43:27 -0600

    Adding collider to take damage hits in player

--- VR Project 2 Covid Shopping Simulator/Assets/Player.cs
+++ VR Project 2 Covid Shopping Simulator/Assets/Player.cs
@@ -10,13 +10,17 @@ public class Player : MonoBehaviour
 
     public int currentExposure;
 
+    public Collider coll;
+
+
     public ExposureMeter exposureMeter;
 
     void Start()
     {
         currentExposure = maxExposure;
         exposureMeter.setMaxExposure(maxExposure);
-        
+        coll = GetComponent<Collider>();
+
     }
 
     void Update()
@@ -40,6 +44,16 @@ public class Player : MonoBehaviour
         currentExposure -= damage;
         exposureMeter.setExposure(currentExposure);
 
+    }
+
+    void ApplyHitPoints(Vector3 explosionPos, float radius)
+    {
+        Vector3 closestPoint = coll.ClosestPointOnBounds(explosionPos);
+        float distance = Vector3.Distance(closestPoint, explosionPos);
+        float damage = 1.0F - Mathf.Clamp01(distance / radius);
+
+        int totalDamage = (int)damage;
+        TakeDamage(totalDamage);
     }
 
 }
